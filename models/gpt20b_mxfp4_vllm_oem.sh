75bc1bbb4430bb5c7ce2c646a1fdb23d02855eba
shaurizard <shaunsingh0207@gmail.com>
2025-12-14 14:07:52 -0500

    tf32

--- models/gpt20b_mxfp4_vllm_oem.sh
+++ models/gpt20b_mxfp4_vllm_oem.sh
@@ -15,7 +15,7 @@ docker run \
   -e VLLM_FLASHINFER_MOE_BACKEND="${VLLM_FLASHINFER_MOE_BACKEND:-throughput}" \
   -e FLASHINFER_LOGGING_LEVEL="${FLASHINFER_LOGGING_LEVEL:-error}" \
   vllm:intellect3 \
-  --model openai/gpt-oss-20b \
+  --model 2imi9/gpt-oss-20B-NVFP4A16-BF16 \
   --async-scheduling \
   --gpu-memory-utilization 0.4 \
   --speculative-config '{"model":"RedHatAI/gpt-oss-20b-speculator.eagle3","num_speculative_tokens":3,"draft_tensor_parallel_size":1}' \
